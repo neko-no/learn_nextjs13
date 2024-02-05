5e017ff8206dbd05cb7050381c09380c1d13c996
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-05 17:44:49 +0900

    #7 add: NotFoundページを作成;

---
+++ nextjs13-tailwindcss/src/app/articles/not-found.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <div className="p-9 rounded-lg text-center">
+        <h1>404</h1>
+        <p>ページが見つかりませんでした．</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;

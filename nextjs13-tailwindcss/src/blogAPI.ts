59989fb431f6637492f1f60e958f5ae18ad03a1b
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-05 15:30:21 +0900

    #7 add: React Error Boundaryを用いたエラーページの作成

--- nextjs13-tailwindcss/src/blogAPI.ts
+++ nextjs13-tailwindcss/src/blogAPI.ts
@@ -5,6 +5,10 @@ export const getAllAritcles = async(): Promise<Article[]>  => {
     // SSRでの取得を行う
     const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store"});
 
+    if(!res.ok){
+        throw new Error('エラーが発生しました')
+    }
+
     const articles = await res.json();
     return articles;
 };

3e3a66c25891e50d3a8c9be4e3949406c26795ae
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-05 17:37:02 +0900

    #7 add: Articleの記述をコンポーネントに切り分ける

---
+++ nextjs13-tailwindcss/src/app/Components/ArticleCard.tsx
@@ -0,0 +1,55 @@
+import { Article } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type ArticleCardProps = {
+  article: Article;
+};
+
+const ArticleCard = ({ article }: ArticleCardProps) => {
+  return (
+    <div>
+      <article key={article.id} className="shadow my-5">
+        <Link href={`articles/${article.id}`} className="hover:opacity-75">
+          <Image
+            width="1000"
+            height="500"
+            src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
+            alt=""
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-5">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href={`articles/${article.id}`}
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            {article.title}
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">
+            By neko-no, published {article.createdAt}
+          </p>
+          <Link
+            href={`articles/${article.id}`}
+            className="text-slate-900 pb-6 "
+          >
+            {article.content.length > 70
+              ? article.content.substring(0, 70) + "..."
+              : article.content}
+          </Link>
+          <Link
+            href={`articles/${article.id}`}
+            className="text-pink-700 hover:text-black"
+          >
+            続きを読む
+          </Link>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default ArticleCard;
--- nextjs13-tailwindcss/src/app/Components/ArticleList.tsx
+++ nextjs13-tailwindcss/src/app/Components/ArticleList.tsx
@@ -2,6 +2,7 @@ import { Article } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import ArticleCard from "./ArticleCard";
 
 type ArticleListProps = {
   articles: Article[];
@@ -11,44 +12,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => {
-        return (
-          <article key={article.id} className="shadow my-5">
-            <Link href={`articles/${article.id}`} className="hover:opacity-75">
-              <Image
-                width="1000"
-                height="500"
-                src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
-                alt=""
-              />
-            </Link>
-            <div className="bg-white flex flex-col justify-start p-5">
-              <Link href="#" className="text-blue-700 pb-4 font-bold">
-                Technology
-              </Link>
-              <Link
-                href={`articles/${article.id}`}
-                className="text-slate-900 text-3xl font-bold hover: text-gray-700 pb-4"
-              >
-                {article.title}
-              </Link>
-              <p className="text-sm pb-3 text-slate-900">
-                By neko-no, published {article.createdAt}
-              </p>
-              <Link
-                href={`articles/${article.id}`}
-                className="text-slate-900 pb-6 "
-              >
-                {article.content}
-              </Link>
-              <Link
-                href={`articles/${article.id}`}
-                className="text-pink-700 hover:text-black"
-              >
-                続きを読む
-              </Link>
-            </div>
-          </article>
-        );
+        return <ArticleCard key={article.id} article={article} />;
       })}
     </div>
   );

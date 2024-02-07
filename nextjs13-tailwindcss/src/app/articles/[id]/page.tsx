9dbb083c17cc3c6a83f008977f40c546861dd340
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-08 00:02:11 +0900

    #9 add: detail記事を取得

--- nextjs13-tailwindcss/src/app/articles/[id]/page.tsx
+++ nextjs13-tailwindcss/src/app/articles/[id]/page.tsx
@@ -4,9 +4,13 @@ import Image from "next/image";
 import React from "react";
 
 const Article = async ({ params }: { params: { id: string } }) => {
-  const detailArticle = await getDetailAritcle(params.id);
-
-  const handleDelete = async () => {};
+  // const detailArticle = await getDetailAritcle(params.id);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${API_URL}/api/${params.id}`, {
+    next: { revalidate: 10 },
+  });
+  const detailArticle = await res.json();
+  console.log(detailArticle);
 
   return (
     <div className="max-w-3xl mx-auto p-5">

659dccfaec1134d318af1a3172b870d8c9b93332
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-09 20:40:34 +0900

    #9 add: 新規作成APIの作成;

--- nextjs13-tailwindcss/src/app/api/blog/route.ts
+++ nextjs13-tailwindcss/src/app/api/blog/route.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/utils/supabaseClient";
 import { NextApiRequest, NextApiResponse } from "next";
+import { notFound } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, res: Response) {
@@ -10,4 +11,19 @@ export async function GET(req: Request, res: Response) {
     }
 
     return NextResponse.json(data, {status: 200})
+}
+
+export async function POST(req: Request, res: Response) {
+    const {id, title, content} = await req.json();
+    const {data, error } = await supabase.from('posts').insert([{id, title, content,createdAt: new Date().toISOString()}])
+
+    if(error) {
+        return NextResponse.json({error:error.message})
+    }
+
+    if(!data){
+        notFound();
+    }
+
+    return NextResponse.json(data, { status: 201})
 }
